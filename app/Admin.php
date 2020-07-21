b0e373eb3c1e1d60f2dd1e91cf14ae30fbe7ce30
Andy Parinas <andy.parinas@gmail.com>
2020-07-21 17:56:40 +1000

    Implemented and Tested Sanctum for MultiAuth

--- app/Admin.php
+++ app/Admin.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     protected $guard = 'admin';
 
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Admin;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('admin-only', function($user){
+            //return $user->tokenCan('admin');
+            return $user instanceof Admin;
+        });
+
     }
 }
