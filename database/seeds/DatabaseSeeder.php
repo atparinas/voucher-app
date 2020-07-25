3c7072f8a58f3b2648d4342c3666af4bc25ae49b
Andy Parinas <parinasadolfo@gmail.com>
2020-07-25 16:45:04 +1000

    Created Seeder for User

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        factory(\App\User::class, 20)->create()
+            ->each(function ($user){
+               $voucherCount = rand(1, 10);
+
+               factory(\App\Voucher::class, $voucherCount)->create(['user_id' => $user->id]);
+
+            });
     }
 }
